8c06c6c80343aea35080ca693a260763520c61fc
Sr. Mies (Nacho) <ignaciomies@live.com.ar>
2018-09-29 13:56:27 -0300

    Desafio 13

---
+++ Desafio13/Desafio13.js
@@ -0,0 +1,27 @@
+function getData (url, callback) {
+    $.ajax(url)
+      .done(function (data) {
+        callback(null, data)
+        if(data.next != null)
+            getData(data.next, function(){                
+                showNames(data);
+            });
+            if(data.results){
+                showNames(data);
+            }
+      })
+      .fail(function (error) {
+        callback(error)
+      })
+  }
+
+  getData("https://swapi.co/api/people/", function(status, data){
+        
+  });
+
+  function showNames(data){
+    for(var i = 0; i < data.results.length; i++)
+    {
+        console.log(data.results[i].name);
+    }
+  }
